5cb7ca83d76b276e0c96303d7e885842262cfba6
Dheeru Singh <dheeru.singh228@gmail.com>
2019-08-14 00:11:55 -0400

    ready to depl

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var friends = require("./app/data/friends.js");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 5000;
+var PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + "/app/css"));
 
